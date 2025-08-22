8310777b68783c8dd07c639221dc7fb3f36c7b2e
eclipxia <xaylei@outlook.com>
2025-08-22 12:21:10 -0400

    feat: terminer partie8

---
+++ partie8.py
@@ -0,0 +1,31 @@
+#!/opt/homebrew/bin/python3 
+import turtle
+t = turtle.Turtle()
+t.color("yellow")
+t.width(10)
+
+t.left(90)
+def etoille():
+    for _ in range(5):
+        t.forward(50)
+        t.right(144)
+    t.penup()
+    t.forward(150)
+    t.pendown()
+
+for _ in range(3):
+    etoille()
+    t.right(120)
+
+t.left(90)
+t.penup()
+t.forward(150)
+t.pendown()
+t.color("black")
+
+for _ in range(360):
+    t.forward(50)
+    t.backward(50)
+    t.right(1)
+
+turtle.done()
